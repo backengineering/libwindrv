ee8d63322064d4ac30f23fa57d107a0115e17dcf
gmh5225 <2315157@qq.com>
2023-10-28 22:38:22 +0800

    Update

--- src/libwindrv.cpp
+++ src/libwindrv.cpp
@@ -1,1 +1,91 @@
 #include <libwindrv/libwindrv.h>
+
+#include <ntimage.h>
+
+#define NTSTRSAFE_LIB
+#define NTSTRSAFE_NO_CB_FUNCTIONS
+#include <ntstrsafe.h>
+
+typedef struct _KLDR_DATA_TABLE_ENTRY_COMMON
+{
+    LIST_ENTRY InLoadOrderLinks;
+    PVOID ExceptionTable;
+    ULONG ExceptionTableSize;
+    // ULONG padding on IA64
+    PVOID GpValue;
+    PNON_PAGED_DEBUG_INFO NonPagedDebugInfo;
+    PVOID DllBase;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+    ULONG Flags;
+    USHORT LoadCount;
+    USHORT __Unused5;
+    PVOID SectionPointer;
+    ULONG CheckSum;
+    // ULONG padding on IA64
+    PVOID LoadedImports;
+    PVOID PatchInformation;
+} KLDR_DATA_TABLE_ENTRY_COMMON, *PKLDR_DATA_TABLE_ENTRY_COMMON;
+
+PDRIVER_OBJECT LibWinDrvDriverObject = nullptr;
+wchar_t LibWinDrvRegistryKey[_MAX_PATH];
+wchar_t LibWinDrvServiceKeyName[_MAX_PATH];
+PVOID LibWinDrvImageBase = nullptr;
+SIZE_T LibWinDrvImageSize = 0;
+
+void
+OnUnload(__in DRIVER_OBJECT *driverObject)
+{
+    UNREFERENCED_PARAMETER(driverObject);
+
+    LibWinDrvDriverUnLoad(driverObject);
+
+    // TODO
+}
+
+EXTERN_C
+NTSTATUS
+DriverEntry(__in DRIVER_OBJECT *DriverObject, __in UNICODE_STRING *RegistryPath)
+{
+    NTSTATUS Status = STATUS_UNSUCCESSFUL;
+    // TODO
+
+    DriverObject->DriverUnload = (DRIVER_UNLOAD *)(OnUnload);
+    LibWinDrvDriverObject = DriverObject;
+
+    do
+    {
+        KLDR_DATA_TABLE_ENTRY_COMMON *pEntry = (KLDR_DATA_TABLE_ENTRY_COMMON *)(LibWinDrvDriverObject->DriverSection);
+        if (pEntry)
+        {
+            LibWinDrvImageBase = pEntry->DllBase;
+            LibWinDrvImageSize = (SIZE_T)pEntry->SizeOfImage;
+
+            // TODO
+
+            DriverObject->Flags |= 0x20;
+        }
+        else
+        {
+            Status = STATUS_VIRUS_DELETED;
+            break;
+        }
+
+        if (RegistryPath)
+        {
+            // TODO
+        }
+        else
+        {
+            Status = STATUS_VIRUS_INFECTED;
+            break;
+        }
+
+        Status = LibWinDrvDriverEntry(DriverObject, RegistryPath);
+
+    } while (0);
+
+    return Status;
+}
