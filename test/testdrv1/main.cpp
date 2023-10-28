23ef6a6aa4f1090762b1ad1b28ddb55acf7d7d7f
gmh5225 <2315157@qq.com>
2023-10-28 22:41:38 +0800

    Update

--- test/testdrv1/main.cpp
+++ test/testdrv1/main.cpp
@@ -1,1 +1,15 @@
 #include <libwindrv/libwindrv.h>
+
+EXTERN_C
+void
+LibWinDrvDriverUnLoad(__in DRIVER_OBJECT *driverObject)
+{
+    UNREFERENCED_PARAMETER(driverObject);
+}
+
+EXTERN_C
+NTSTATUS
+LibWinDrvDriverEntry(__in DRIVER_OBJECT *DriverObject, __in UNICODE_STRING *RegistryPath)
+{
+    return -1;
+}
