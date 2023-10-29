0f09c13abea280e79c144c38d32fd45ac4b8b888
gmh5225 <2315157@qq.com>
2023-10-29 16:48:01 +0800

    Uoload enable_cet.bat

---
+++ tools/enable_cet.bat
@@ -0,0 +1,3 @@
+reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity /v Enabled /t REG_DWORD /d 1 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\KernelShadowStacks /v Enabled /t REG_DWORD /d 1 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\KernelShadowStacks /v AuditModeEnabled /t REG_DWORD /d 1 /f
