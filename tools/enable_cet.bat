6755691a061fb4a36adaf183f4219ff3d1ab9335
gmh <13917777+gmh5225@users.noreply.github.com>
2023-11-10 23:35:52 +0800

    Delete tools directory

--- tools/enable_cet.bat
+++
@@ -1,3 +0,0 @@
-reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity /v Enabled /t REG_DWORD /d 1 /f
-reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\KernelShadowStacks /v Enabled /t REG_DWORD /d 1 /f
-reg add HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\KernelShadowStacks /v AuditModeEnabled /t REG_DWORD /d 1 /f
